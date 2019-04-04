57c0e10268166984b376d8f09ba74cb831a00dd8
Veljko Lopicic <vlopicic@gmail.com>
2019-04-04 02:34:30 +0200

    home page karusel i kreiranje baze

---
+++ BioskopApp/BioskopApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BioskopApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using BioskopApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioskopApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext dbContext {get; set; }
+
+        public HomeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+
+            ViewData["Movies"] = dbContext.Movies.Where(m => m.Highlighted);
+         
+                
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BioskopApp/BioskopApp/Data/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BioskopApp.Data
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public bool Highlighted { get; set; }
+
+        public string Description { get; set; }
+
+        public string Trailer { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public string ImageRepertoar { get; set; }
+    }
+}
